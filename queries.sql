482d634c8be96b61a34ef302369ea5787b93907e
vanina123 <nanavanina834@gmail>
2023-06-14 17:46:31 +0100

    query and update animals table

--- queries.sql
+++ queries.sql
@@ -18,3 +18,64 @@ SELECT * FROM animals WHERE name Like '%mon';
 
 --  Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3 ;
+
+-- Inside a transaction update the animals table by setting the species column to unspecified. Verify that change was made. Then roll back the change and verify that the species columns went back to the state before the transaction.
+- BEGIN TRANSACTION;
+- UPDATE animals SET species = 'unspecified';
+- SELECT species FROM animals;
+- ROLLBACK;
+- SELECT * FROM animals;
+
+-- Inside a transaction:
+-- Update the animals table by setting the species column to digimon for all animals that have a name ending in mon.
+-- Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+-- Verify that changes were made.
+-- Commit the transaction.
+-- Verify that changes persist after commit.
+
+- BEGIN TRANSACTION;
+- UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+-  UPDATE animals SET species = 'pokemon' WHERE species is null;
+- SELECT * FROM animals;
+- COMMIT;
+- SELECT * FROM animals;
+
+-- Inside a transaction delete all records in the animals table, then roll back the transaction.
+- BEGIN TRANSACTION;
+- DELETE FROM animals;
+- ROLLBACK;
+- SELECT * FROM animals;
+
+-- Delete all animals born after Jan 1st, 2022.
+-- Create a savepoint for the transaction.
+-- Update all animals' weight to be their weight multiplied by -1.
+-- Rollback to the savepoint
+-- Update all animals' weights that are negative to be their weight multiplied by -1.
+-- Commit transaction
+- BEGIN TRANSACTION;
+- DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+- SAVEPOINT SP1;
+- UPDATE animals SET weight_kg = weight_kg * -1;
+- ROLLBACK TO SP1;
+- UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+- COMMIT;
+- SELECT * FROM animals;
+
+-- How many animals are there?
+- SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+- SELECT COUNT(*) FROM animals WHERE escape_attemps = 0;
+
+-- What is the average weight of animals?
+- SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutred, COUNT(1) AS escape_count FROM animals GROUP BY neutred;
+
+-- What is the minimum and maximum weight of each type of animal?
+- SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+
+SELECT species, AVG(escape_attemps) AS avg_escape_attemps FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
--- schema.sql
+++ schema.sql
@@ -6,3 +6,16 @@ CREATE TABLE animals (
     neutred BOOLEAN,
     weight_kg DECIMAL
 );
+-- day 2
+-- Add a column species of type string to your animals table. Modify your schema.sql file.
+ALTER TABLE animals ADD species VARCHAR(255);
+
+CREATE TABLE animals (
+    id int not null,
+    name VARCHAR not null,
+    date_of_birth DATE,
+    escape_attemps INT,
+    neutred BOOLEAN,
+    weight_kg DECIMAL,
+    species VARCHAR(255)
+);
